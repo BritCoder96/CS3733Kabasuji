750a2567b638a7a8ac1c84f0c5a72cfcae2463f0
higoset <ejcerini@wpi.edu>
2016-04-09 16:46:49 -0400

    Adding controllers for moving between builder screens. Also I learned how to Github, so that's fun.

---
+++ src/main/KabuildsujiMain.java
@@ -0,0 +1,22 @@
+package main;
+import java.awt.Rectangle;
+
+import javax.swing.JFrame;
+
+public class KabuildsujiMain {
+
+	public static Rectangle windowSize = new Rectangle(100, 100, 800, 650);
+
+	private static JFrame frame;
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		frame = new JFrame();
+		frame.setVisible(true);
+		frame.setBounds(KabuildsujiMain.windowSize);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setContentPane(new views.BuilderTitle(frame));
+	}
+}
--- src/views/BuilderTitle.java
+++ src/views/BuilderTitle.java
@@ -6,6 +6,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import controllers.MoveToBuilderLevelListController;
 import main.KabasujiMain;
 
 import javax.swing.JLabel;
@@ -41,6 +42,7 @@ public class BuilderTitle extends JPanel {
 		btnEdit.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		btnEdit.setBounds(338, 305, 120, 45);
 		add(btnEdit);
+		btnEdit.addActionListener(new MoveToBuilderLevelListController(frame, this));
 
 		
 	}
