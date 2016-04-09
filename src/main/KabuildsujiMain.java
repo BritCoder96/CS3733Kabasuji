8ee74d9e0140e78a56c0e4de76a7bad58c5c3fea
Benjamin Huchley <Benjamin Huchley>
2016-04-09 17:02:31 -0400

    added framework for back panel manager

--- src/main/KabuildsujiMain.java
+++ src/main/KabuildsujiMain.java
@@ -1,22 +1,24 @@
 package main;
 import java.awt.Rectangle;
 
-import javax.swing.JFrame;
+import views.KabasujiFrame;
 
 public class KabuildsujiMain {
 
 	public static Rectangle windowSize = new Rectangle(100, 100, 800, 650);
 
-	private static JFrame frame;
+	private static KabasujiFrame frame;
+	private static PanelBackManager backMgr;
 	
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		frame = new JFrame();
+		backMgr = new PanelBackManager();
+		frame = new KabasujiFrame(backMgr);
 		frame.setVisible(true);
 		frame.setBounds(KabuildsujiMain.windowSize);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setDefaultCloseOperation(KabasujiFrame.EXIT_ON_CLOSE);
 		frame.setContentPane(new views.BuilderTitle(frame));
 	}
 }
