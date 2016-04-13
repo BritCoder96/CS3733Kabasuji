2ea0c810161274f8b7854868f4132ad7b25f6464
higoset <ejcerini@wpi.edu>
2016-04-13 14:23:14 -0400

    Whoops forgot to push BuilderTitle

--- src/views/BuilderTitle.java
+++ src/views/BuilderTitle.java
@@ -42,6 +42,36 @@ public class BuilderTitle extends JPanel {
 		btnEdit.setBounds(338, 305, 120, 45);
 		add(btnEdit);
 		btnEdit.addActionListener(new MoveToBuilderLevelListController(frame, this));
+		
+		JLabel label = new JLabel("Euphorbus");
+		label.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		label.setBounds(321, 433, 155, 39);
+		add(label);
+		
+		JLabel label_1 = new JLabel("Bryan Benson");
+		label_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		label_1.setBounds(337, 474, 123, 27);
+		add(label_1);
+		
+		JLabel label_2 = new JLabel("Jon Berry");
+		label_2.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		label_2.setBounds(357, 501, 83, 27);
+		add(label_2);
+		
+		JLabel label_3 = new JLabel("Erik Cerini");
+		label_3.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		label_3.setBounds(353, 528, 90, 27);
+		add(label_3);
+		
+		JLabel label_4 = new JLabel("Ben Huchley");
+		label_4.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		label_4.setBounds(343, 555, 111, 27);
+		add(label_4);
+		
+		JLabel label_5 = new JLabel("Steven Huynh");
+		label_5.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		label_5.setBounds(335, 582, 127, 27);
+		add(label_5);
 
 		
 	}
