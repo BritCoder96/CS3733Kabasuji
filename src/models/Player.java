b682173793e828e310a124a2d631f3435852308a
Steven Huynh <c68280@trbvn.com>
2016-04-16 02:39:49 -0400

    Added models/Player class

---
+++ src/models/Player.java
@@ -0,0 +1,32 @@
+package models;
+
+public class Player {
+	// eager implementation of the singleton pattern
+	
+	// TODO: use some other Level array for the parameter
+	static final Player inst = new Player(new Level[15]);
+	
+	Level[] levels = new Level[15];
+	
+	Level currentLevel;
+	
+	Player(Level[] levels) {
+		this.levels = levels;
+	}
+	
+	public Player instance() {
+		return inst;
+	}
+	
+	public void playLevel(int levelNumber) {
+		for(Level i : levels) {
+			if(i.levelNumber == levelNumber){
+				currentLevel = i;
+				// TODO: more stuff?
+				return;
+			}
+		}
+		// must play a level that exists
+		throw new IllegalArgumentException();
+	}
+}
