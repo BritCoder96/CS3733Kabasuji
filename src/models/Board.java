77d273f4b3decbb7bc89f71ca125d854b163aede
Bryan Benson <bryanbenson83@gmail.com>
2016-04-19 19:15:01 -0400

    Comment most models and implement rotatePiece

--- src/models/Board.java
+++ src/models/Board.java
@@ -1,30 +1,64 @@
 package models;
 import java.util.HashSet;
 
-
+/**
+ * The generic board for all levels.s
+ * 
+ * @author bjbenson
+ * @author jberry
+ * 
+ */
 public class Board {
+	/** The number of rows in the board */
 	int rows;
+	/** The number of columns in the board */
 	int columns;
+	/** The squares that make up the board */
 	HashSet<Square> squares;
+	/** The pieces that are on the board */
 	HashSet<Piece> pieces;
 	
+	
+	/**
+	 * Board Constructor
+	 * 
+	 * @param rows
+	 * @param columns
+	 */
 	Board (int rows, int columns) {
 		this.rows = rows;
 		this.columns = columns;
 	}
 	
+	/** Get the board's pieces
+	 * 
+	 * @return the pieces on the board
+	 * */
 	HashSet<Piece> getPieces() {
 		return pieces;
 	}
-	
+	/** Get the board's squares
+	 * 
+	 * @return the squares on the board
+	 */
 	HashSet<Square> getSquares() {
 		return squares;
 	}
 	
+	/** 
+	 * Set the board's squares
+	 * 
+	 * @param squares
+	 */
 	void setSquares(HashSet<Square> squares) {
 		this.squares = squares;
 	}
 	
+	/**
+	 * Set the board's pieces
+	 * 
+	 * @param pieces
+	 */
 	void setPieces(HashSet<Piece> pieces) {
 		this.pieces = pieces;
 	}
--- src/models/SquareTypes.java
+++ src/models/SquareTypes.java
@@ -1,5 +1,11 @@
 package models;
 
+/**
+ * The types of a square.
+ * 
+ * @author bjbenson
+ *
+ */
 public enum SquareTypes {
 	PIECESQUARE, BOARDSQUARE;
 }
