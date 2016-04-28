ab732721ab81aa5fe14c8d3338298df9e677ac45
Benjamin Huchley <Benjamin Huchley>
2016-04-28 12:36:44 -0400

    Fixed the annoying error where the splash screen would be offset

--- src/main/KabasujiMain.java
+++ src/main/KabasujiMain.java
@@ -5,6 +5,11 @@ import javax.swing.JFrame;
 
 import views.KabasujiFrame;
 
+/**
+ * The class that launches the game.
+ * @author bhuchley
+ *
+ */
 public class KabasujiMain {
 
 	public static Rectangle windowSize = new Rectangle(100, 100, 800, 650);
@@ -18,10 +23,9 @@ public class KabasujiMain {
 	public static void main(String[] args) {
 		backMgr = new PanelBackManager();
 		frame = new KabasujiFrame(backMgr);
+		frame.setContentPane(new views.Title(frame));
 		frame.setVisible(true);
 		frame.setBounds(KabasujiMain.windowSize);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setContentPane(new views.Title(frame));
-		System.out.println("Working Directory = " + System.getProperty("user.dir"));
 	}
 }
--- src/main/KabuildsujiMain.java
+++ src/main/KabuildsujiMain.java
@@ -3,6 +3,11 @@ import java.awt.Rectangle;
 
 import views.KabasujiFrame;
 
+/**
+ * The class that launches the builder.
+ * @author bhuchley
+ *
+ */
 public class KabuildsujiMain {
 
 	public static Rectangle windowSize = new Rectangle(100, 100, 800, 650);
@@ -16,10 +21,10 @@ public class KabuildsujiMain {
 	public static void main(String[] args) {
 		backMgr = new PanelBackManager();
 		frame = new KabasujiFrame(backMgr);
+		frame.setContentPane(new views.BuilderTitle(frame));
 		frame.setVisible(true);
 		frame.setBounds(KabuildsujiMain.windowSize);
 		frame.setDefaultCloseOperation(KabasujiFrame.EXIT_ON_CLOSE);
-		frame.setContentPane(new views.BuilderTitle(frame));
 		
 	}
 }
